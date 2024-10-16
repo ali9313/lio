953e61cb2cee7e198a584276d82a20a25070391a
ali9313 <lioali65808@gmail.com>
2024-10-17 00:03:35 +0300

    Add files via upload

---
+++ aliup/assistant/secret.py
@@ -0,0 +1,28 @@
+import json
+import os
+import re
+
+from telethon.events import CallbackQuery
+
+from aliup import l313l
+
+
+@l313l.tgbot.on(CallbackQuery(data=re.compile(b"rzan_(.*)")))
+async def on_plug_in_callback_query_handler(event):
+    timestamp = int(event.pattern_match.group(1).decode("UTF-8"))
+    if os.path.exists("./aliup/secrets.txt"):
+        jsondata = json.load(open("./aliup/secrets.txt"))
+        try:
+            message = jsondata[f"{timestamp}"]
+            userid = message["userid"]
+            ids = [userid, l313l.uid]
+            if event.query.user_id in ids:
+                encrypted_tcxt = message["text"]
+                reply_pop_up_alert = encrypted_tcxt
+            else:
+                reply_pop_up_alert = "⌯︙عـذرا هذه الهـمسة ليست مخصصة لـك"
+        except KeyError:
+            reply_pop_up_alert = "⌯︙عـذرا هذه الهمسة لم تعد موجوده في سيـرفرات ꪖꪶ𝓲"
+    else:
+        reply_pop_up_alert = "⌯︙عـذرا هذه الهمسة لم تعد موجوده  "
+    await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
