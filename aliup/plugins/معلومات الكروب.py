f934693916344178af7af8aaf68c9f2eeed8d311
ali9313 <lioali65808@gmail.com>
2024-10-17 19:08:06 +0300

    Add files via upload

--- aliup/plugins/معلومات الكروب.py
+++ aliup/plugins/معلومات الكروب.py
@@ -18,7 +18,7 @@ from telethon.tl.types import (
 )
 from telethon.utils import get_input_location
 
-from JoKeRUB import l313l
+from aliup import l313l
 
 from ..core.logger import logging
 from ..core.managers import edit_delete, edit_or_reply
@@ -39,12 +39,12 @@ plugin_category = "utils"
             "{tr}المشرفيـن +إسم المستخـدم/معرّف المستخـدم> ✪",
             "{tr}المشرفيـن + في المجموعـة التي تريدهـا> ✪",
         ],
-        "examples": "{tr}المشرفين @l313l",
+        "examples": "{tr}المشرفين @u_gg_u",
     },
 )
 async def _(event):
     "لإظهـار قائمـة المشرفيـن  ✪"
-    mentions = "**᯽︙ مشرفيـن هـذه المجموعـة  ✪**: \n"
+    mentions = "** مشرفيـن هـذه المجموعـة  ✪**: \n"
     reply_message = await reply_id(event)
     input_str = event.pattern_match.group(1)
     to_write_chat = await event.get_input_chat()
@@ -58,7 +58,7 @@ async def _(event):
     else:
         chat = to_write_chat
         if not event.is_group:
-            return await edit_or_reply(event, "**᯽︙ هـذه ليسـت مجموعـة ✕**")
+            return await edit_or_reply(event, "** هـذه ليسـت مجموعـة ✕**")
     try:
         async for x in event.client.iter_participants(
             chat, filter=ChannelParticipantsAdmins
@@ -88,23 +88,23 @@ async def _(event):
     pattern="البوتات(?: |$)(.*)",
     command=("البوتات", plugin_category),
     info={
-        "header": "᯽︙ لإظهـار قائمـة البوتـات 🝰",
-        "description": "᯽︙ سيظهـر لك قائمـة البوتـات  🝰",
+        "header": " لإظهـار قائمـة البوتـات 🝰",
+        "description": " سيظهـر لك قائمـة البوتـات  🝰",
         "usage": [
             "{tr}البوتات + إسم المستخـدم/معرّف المستخـدم> 🝰 ",
             "{tr}البوتات + في المجموعـة التي تريدهـا 🝰 ",
         ],
-        "examples": "{tr}البوتات @l313l",
+        "examples": "{tr}البوتات @u_gg_u",
     },
 )
 async def _(event):
-    "᯽︙ لإظهـار قائمـة البوتـات 🝰"
-    mentions = "**᯽︙ البـوتات في هذه الـمجموعة 🝰 : ** \n"
+    " لإظهـار قائمـة البوتـات 🝰"
+    mentions = "** البـوتات في هذه الـمجموعة 🝰 : ** \n"
     input_str = event.pattern_match.group(1)
     if not input_str:
         chat = await event.get_input_chat()
     else:
-        mentions = "**᯽︙ البوتـات في {} من المجموعات 🝰 : ** \n".format(input_str)
+        mentions = "** البوتـات في {} من المجموعات 🝰 : ** \n".format(input_str)
         try:
             chat = await event.client.get_entity(input_str)
         except Exception as e:
@@ -140,25 +140,25 @@ async def _(event):
     },
 )
 async def get_users(show):
-    "᯽︙ لإظهـار قائمـة الأعضـاء 𖤍"
+    " لإظهـار قائمـة الأعضـاء 𖤍"
     mentions = "**مستخدمين هذه المجموعة**: \n"
     await reply_id(show)
     input_str = show.pattern_match.group(1)
     if input_str:
-        mentions = "**᯽︙ الأعضاء في {} من المجموعات 𖤍  :** \n".format(input_str)
+        mentions = "** الأعضاء في {} من المجموعات 𖤍  :** \n".format(input_str)
         try:
             chat = await show.client.get_entity(input_str)
         except Exception as e:
             return await edit_delete(show, f"`{str(e)}`", 10)
     else:
         if not show.is_group:
-            return await edit_or_reply(show, "**᯽︙ هـذه ليسـت مجموعـة ✕**")
-    catevent = await edit_or_reply(show, "**᯽︙ جـاري سحـب قائمـة معرّفـات الأعضـاء 🝛**")
+            return await edit_or_reply(show, "** هـذه ليسـت مجموعـة ✕**")
+    catevent = await edit_or_reply(show, "** جـاري سحـب قائمـة معرّفـات الأعضـاء 🝛**")
     try:
         if show.pattern_match.group(1):
             async for user in show.client.iter_participants(chat.id):
                 if user.deleted:
-                    mentions += f"\n**᯽︙ الحسـابات المحذوفـة ⌦** `{user.id}`"
+                    mentions += f"\n** الحسـابات المحذوفـة ⌦** `{user.id}`"
                 else:
                     mentions += (
                         f"\n[{user.first_name}](tg://user?id={user.id}) `{user.id}`"
@@ -166,7 +166,7 @@ async def get_users(show):
         else:
             async for user in show.client.iter_participants(show.chat_id):
                 if user.deleted:
-                    mentions += f"\n**᯽︙ الحسـابات المحذوفـة ⌦** `{user.id}`"
+                    mentions += f"\n** الحسـابات المحذوفـة ⌦** `{user.id}`"
                 else:
                     mentions += (
                         f"\n[{user.first_name}](tg://user?id={user.id}) `{user.id}`"
@@ -190,7 +190,7 @@ async def get_users(show):
     },
 )
 async def info(event):
-    "᯽︙ للحصـول على معلومـات المجموعـة 🝢"
+    " للحصـول على معلومـات المجموعـة 🝢"
     if not event.is_group:
         return await edit_delete(event, "**لا يمكن استعمال الأمر سوى في المجموعات**")
     catevent = await edit_or_reply(event, "**⌔︙يتـمّ جلـب معلومـات الدردشـة، إنتظـر ⅏**")
@@ -201,9 +201,9 @@ async def info(event):
     except Exception as e:
         if BOTLOG:
             await event.client.send_message(
-                BOTLOG_CHATID, f"**᯽︙ هنـاك خطـأ في معلومـات الدردشـة ✕ : **\n`{str(e)}`"
+                BOTLOG_CHATID, f"** هنـاك خطـأ في معلومـات الدردشـة ✕ : **\n`{str(e)}`"
             )
-        await catevent.edit("**᯽︙ حـدث خـطأ مـا، يرجـى التحقق من الأمـر ⎌**")
+        await catevent.edit("** حـدث خـطأ مـا، يرجـى التحقق من الأمـر ⎌**")
 
 
 async def get_chatinfo(event, catevent):
@@ -227,15 +227,15 @@ async def get_chatinfo(event, catevent):
         try:
             chat_info = await event.client(GetFullChannelRequest(chat))
         except ChannelInvalidError:
-            await catevent.edit("**᯽︙ لـم يتـمّ العثـور على القنـاة/المجموعـة ✕**")
+            await catevent.edit("** لـم يتـمّ العثـور على القنـاة/المجموعـة ✕**")
             return None
         except ChannelPrivateError:
             await catevent.edit(
-                '**᯽︙ هـذه مجموعـة أو قنـاة خاصـة أو لقد تمّ حظـري منه ⛞**'
+                '** هـذه مجموعـة أو قنـاة خاصـة أو لقد تمّ حظـري منه ⛞**'
             )
             return None
         except ChannelPublicGroupNaError:
-            await catevent.edit("**᯽︙ القنـاة أو المجموعـة الخارقـة غيـر موجـودة ✕**")
+            await catevent.edit("** القنـاة أو المجموعـة الخارقـة غيـر موجـودة ✕**")
             return None
         except (TypeError, ValueError) as err:
             await catevent.edit(str(err))
@@ -387,54 +387,54 @@ async def fetch_info(chat, event):  # sourcery no-metrics
         for _ in bots_list:
             bots += 1  
 
-    caption = "<b>᯽︙ معلومـات الدردشـة  🝢 :</b>\n"
+    caption = "<b> معلومـات الدردشـة  🝢 :</b>\n"
     caption += f"⌔︙الآيـدي  : <code>{chat_obj_info.id}</code>\n"
     if chat_title is not None:
-        caption += f"᯽︙ إسـم المجموعـة  :{chat_title}\n"
+        caption += f" إسـم المجموعـة  :{chat_title}\n"
     if former_title is not None:  # Meant is the very first title
-        caption += f"᯽︙ الإسم السابـق  : {former_title}\n"
+        caption += f" الإسم السابـق  : {former_title}\n"
     if username is not None:
-        caption += f"᯽︙ نـوع المجموعـة ⌂ : مجموعـة عامّـة  \n"
-        caption += f"᯽︙ الرابـط  : \n {username}\n"
+        caption += f" نـوع المجموعـة ⌂ : مجموعـة عامّـة  \n"
+        caption += f" الرابـط  : \n {username}\n"
     else:
-        caption += f"᯽︙ نـوع المجموعـة ⌂ : مجموعـة عامّـة  \n"
+        caption += f" نـوع المجموعـة ⌂ : مجموعـة عامّـة  \n"
     if creator_username is not None:
-        caption += f"᯽︙ المالـك  :  {creator_username}\n"
+        caption += f" المالـك  :  {creator_username}\n"
     elif creator_valid:
         caption += (
-            '᯽︙ المالـك  : <a href="tg://user?id={creator_id}">{creator_firstname}</a>\n'
+            ' المالـك  : <a href="tg://user?id={creator_id}">{creator_firstname}</a>\n'
         )
     if created is not None:
-        caption += f"᯽︙ تاريـخ الإنشـاء  : \n <code>{created.date().strftime('%b %d, %Y')} - {created.time()}</code>\n"
+        caption += f" تاريـخ الإنشـاء  : \n <code>{created.date().strftime('%b %d, %Y')} - {created.time()}</code>\n"
     else:
-        caption += f"᯽︙ الإنتـاج  :   <code>{chat_obj_info.date.date().strftime('%b %d, %Y')} - {chat_obj_info.date.time()}</code> {warn_emoji}\n"
+        caption += f" الإنتـاج  :   <code>{chat_obj_info.date.date().strftime('%b %d, %Y')} - {chat_obj_info.date.time()}</code> {warn_emoji}\n"
     caption += f"⌔︙آ يـدي قاعـدة البيانـات : {dc_id}\n"
     if exp_count is not None:
         chat_level = int((1 + sqrt(1 + 7 * exp_count / 14)) / 2)
-        caption += f"᯽︙ الأعضـاء : <code>{chat_level}</code>\n"
+        caption += f" الأعضـاء : <code>{chat_level}</code>\n"
     if messages_viewable is not None:
-        caption += f"᯽︙ الرسائـل التي يمڪن مشاهدتها : <code>{messages_viewable}</code>\n"
+        caption += f" الرسائـل التي يمڪن مشاهدتها : <code>{messages_viewable}</code>\n"
     if messages_sent:
-        caption += f"᯽︙ الرسائـل المرسلـة  :<code>{messages_sent}</code>\n"
+        caption += f" الرسائـل المرسلـة  :<code>{messages_sent}</code>\n"
     elif messages_sent_alt:
-        caption += f"᯽︙ الرسـائل المرسلة: <code>{messages_sent_alt}</code> {warn_emoji}\n"
+        caption += f" الرسـائل المرسلة: <code>{messages_sent_alt}</code> {warn_emoji}\n"
     if members is not None:
-        caption += f"᯽︙ الأعضـاء : <code>{members}</code>\n"
+        caption += f" الأعضـاء : <code>{members}</code>\n"
     if admins is not None:
-        caption += f"᯽︙ المشرفيـن : <code>{admins}</code>\n"
+        caption += f" المشرفيـن : <code>{admins}</code>\n"
     if bots_list:
-        caption += f"᯽︙ البـوتات : <code>{bots}</code>\n"
+        caption += f" البـوتات : <code>{bots}</code>\n"
     if members_online:
-        caption += f"᯽︙ المتصليـن حـالياً : <code>{members_online}</code>\n"
+        caption += f" المتصليـن حـالياً : <code>{members_online}</code>\n"
     if restrcited_users is not None:
-        caption += f"᯽︙ الأعضـاء المقيّديـن : <code>{restrcited_users}</code>\n"
+        caption += f" الأعضـاء المقيّديـن : <code>{restrcited_users}</code>\n"
     if banned_users is not None:
-        caption += f"᯽︙ الأعضـاء المحظوريـن : <code>{banned_users}</code>"
+        caption += f" الأعضـاء المحظوريـن : <code>{banned_users}</code>"
     if group_stickers is not None:
         caption += f'{chat_type} ⌔︙الملصقـات : <a href="t.me/addstickers/{chat.full_chat.stickerset.short_name}">{group_stickers}</a>'
     caption += "\n"
     if not broadcast:
-        caption += f"᯽︙ الوضـع البطيئ : {slowmode}"
+        caption += f" الوضـع البطيئ : {slowmode}"
         if (
             hasattr(chat_obj_info, "slowmode_enabled")
             and chat_obj_info.slowmode_enabled
@@ -442,19 +442,19 @@ async def fetch_info(chat, event):  # sourcery no-metrics
             caption += f", <code>{slowmode_time}s</code>\n"
         else:
             caption += "\n"
-        caption += f"᯽︙ الـمجموعـة الخارقـة  : {supergroup}\n"
+        caption += f" الـمجموعـة الخارقـة  : {supergroup}\n"
     if hasattr(chat_obj_info, "restricted"):
-        caption += f"᯽︙ المقيّـد : {restricted}"
+        caption += f" المقيّـد : {restricted}"
         if chat_obj_info.restricted:
             caption += f"> : {chat_obj_info.restriction_reason[0].platform}\n"
-            caption += f"> ᯽︙  السـبب  : {chat_obj_info.restriction_reason[0].reason}\n"
-            caption += f"> ᯽︙ النّـص  : {chat_obj_info.restriction_reason[0].text}\n\n"
+            caption += f">   السـبب  : {chat_obj_info.restriction_reason[0].reason}\n"
+            caption += f">  النّـص  : {chat_obj_info.restriction_reason[0].text}\n\n"
         else:
             caption += "\n"
     if hasattr(chat_obj_info, "scam") and chat_obj_info.scam:
         caption += "⌔︙السارقيـن : <b>Yes</b>\n"
     if hasattr(chat_obj_info, "verified"):
-        caption += f"᯽︙ الحسابـات الموثقـة   : {verified}\n"
+        caption += f" الحسابـات الموثقـة   : {verified}\n"
     if description:
         caption += f"⌔︙الوصـف  : \n<code>{description}</code>\n"
     return caption
