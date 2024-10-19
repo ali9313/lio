8d27aa50d8d39df9a001524fdb316a5d9c0e4ff2
ali9313 <lioali65808@gmail.com>
2024-10-19 19:20:41 +0300

    Add files via upload

--- aliup/plugins/تاريخ اسماء.py
+++ aliup/plugins/تاريخ اسماء.py
@@ -1,10 +1,9 @@
-# Copyright (C) 2021 JoKeRUB TEAM
-# FILES WRITTEN BY  @lMl10l
+
 import asyncio
 
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 
-from JoKeRUB import l313l
+from aliup import l313l
 
 from ..core.managers import edit_delete, edit_or_reply
 from ..helpers import get_user_from_event, sanga_seperator
@@ -28,21 +27,21 @@ plugin_category = "utils"
         "examples": "{tr}sg @missrose_bot",
     },
 )
-async def _(event):  #   : @lMl10l
+async def _(event):  
     "To get name/username history."
     input_str = "".join(event.text.split(maxsplit=1)[1:])
     reply_message = await event.get_reply_message()
     if not input_str and not reply_message:
-        await edit_delete( #ترجمه فريق الجوكر على التيلكرام
+        await edit_delete(
             event,
-            "᯽︙  يـجب الـرد على الشخـص او كتـابة معـرفه مع الأمـر لأظـهار تواريـخ اسمـه",
+            "  يـجب الـرد على الشخـص او كتـابة معـرفه مع الأمـر لأظـهار تواريـخ اسمـه",
         )
     user, rank = await get_user_from_event(event, secondgroup=True)
     if not user:
         return
     uid = user.id
     chat = "@SangMataInfo_bot"
-    catevent = await edit_or_reply(event, "᯽︙ انتظر قليلا..")
+    catevent = await edit_or_reply(event, " انتظر قليلا..")
     async with event.client.conversation(chat) as conv:
         try:
             await conv.send_message(f"/search_id {uid}")
@@ -57,9 +56,9 @@ async def _(event):  #   : @lMl10l
             responses.append(response.text)
         await event.client.send_read_acknowledge(conv.chat_id)
     if not responses:
-        await edit_delete(catevent, "᯽︙ لم يتم ايجاد اي نتيجـة")
+        await edit_delete(catevent, " لم يتم ايجاد اي نتيجـة")
     if "No records found" in responses:
-        await edit_delete(catevent, "᯽︙ هـذا المـستخدم لـيس لديـه اي سـجل")
+        await edit_delete(catevent, " هـذا المـستخدم لـيس لديـه اي سـجل")
     names, usernames = await sanga_seperator(responses)
     cmd = event.pattern_match.group(1)
     jasem = None
@@ -67,6 +66,6 @@ async def _(event):  #   : @lMl10l
     for i in check:
         if jasem:
             await event.reply(i, parse_mode=_format.parse_pre)
-        else:#ترجمه فريق الجوكر على التيلكرام
+        else:
             jasem = True
             await catevent.edit(i, parse_mode=_format.parse_pre)
