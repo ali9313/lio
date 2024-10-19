af2b4302a4581e96e59b2c10634844d0570ea96f
ali9313 <lioali65808@gmail.com>
2024-10-19 18:45:30 +0300

    Add files via upload

--- aliup/plugins/صور.py
+++ aliup/plugins/صور.py
@@ -4,7 +4,7 @@ import shutil
 
 from telethon.errors.rpcerrorlist import MediaEmptyError
 
-from JoKeRUB import l313l
+from aliup import l313l
 
 from ..core.managers import edit_or_reply
 from ..helpers.google_image_download import googleimagesdownload
@@ -37,9 +37,9 @@ async def img_sampler(event):
         query = str(event.pattern_match.group(2))
     if not query:
         return await edit_or_reply(
-            event, "** ᯽︙ قم بكتابة النص مع الامر او بالرد على النص **"
+            event, "**  قم بكتابة النص مع الامر او بالرد على النص **"
         )
-    cat = await edit_or_reply(event, "** ᯽︙  جارِ البحث عن الصور انتظر قليلاً ✓ **")
+    cat = await edit_or_reply(event, "**   جارِ البحث عن الصور انتظر قليلاً ✓ **")
     if event.pattern_match.group(1) != "":
         lim = int(event.pattern_match.group(1))
         lim = min(lim, 10)
