31ae4a9974b233b9aa4bd4d4bdd4c60bd908fe37
ali9313 <lioali65808@gmail.com>
2024-10-26 10:50:29 +0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -28,7 +28,7 @@ class Config(object):
     TZ = os.environ.get("TZ", "Asia/Baghdad")
     # set this with required cat repo link
     UPSTREAM_REPO = os.environ.get(
-        "UPSTREAM_REPO", "https://github.com/jepthoniq/jepthon.git"
+        "UPSTREAM_REPO", "https://github.com/ali9313/lio.git"
     )
 
     # BASIC and MAIN CONFIG VARS
