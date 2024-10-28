9bf93b48cb678e7ddf06dbd1ccd6e6a32be0e7fc
ali9313 <lioali65808@gmail.com>
2024-10-28 23:05:07 +0300

    Add files via upload

--- aliup/plugins/الكاشف والبصمه.py
+++ aliup/plugins/الكاشف والبصمه.py
@@ -137,3 +137,31 @@ async def _(e):
             xx += f"\n- [{get_display_name(bb)}](tg://user?id={bb.id})"
     await e.client.send_message(e.chat_id, xx)
     await zel.delete()
+    
+    
+@l313l.ar_cmd(pattern="(تفعيل البصمه الذاتيه|تفعيل البصمه الذاتية|تفعيل البصمة الذاتيه|تفعيل البصمة الذاتية)")
+async def start_datea(event):
+    global vocself
+    if vocself:
+        return await edit_or_reply(event, "**⎉╎حفظ البصمه الذاتية التلقائي 🎙**\n**⎉╎مفعلـه .. مسبقـاً ✅**")
+    vocself = True
+    await edit_or_reply(event, "**⎉╎تم تفعيل حفظ البصمه الذاتية 🎙**\n**⎉╎تلقائياً .. بنجاح ✅**")
+
+@l313l.ar_cmd(pattern="(تعطيل البصمه الذاتيه|تعطيل البصمه الذاتية|تعطيل البصمة الذاتيه|تعطيل البصمة الذاتية)")
+async def stop_datea(event):
+    global vocself
+    if vocself:
+        vocself = False
+        return await edit_or_reply(event, "**⎉╎تم تعطيل حفظ البصمه الذاتية 🎙**\n**⎉╎الان صارت مو شغالة .. ✅**")
+    await edit_or_reply(event, "**⎉╎حفظ البصمه الذاتية التلقائي 🎙**\n**⎉╎معطلـه .. مسبقـاً ✅**")
+
+@l313l.on(events.NewMessage(func=lambda e: e.is_private and (e.audio or e.voice) and e.media_unread))
+async def sddm(event):
+    global vocself
+    zelzal = event.sender_id
+    if vocself:
+        sender = await event.get_sender()
+        username = f"@{sender.username}" if sender.username else "لا يوجد"
+        chat = await event.get_chat()
+        voc = await event.download_media()
+        await l313l.send_file("me", voc, caption=f"حفـظ البصمه الذاتيه \n**⌔ مࢪحبـاً .. عـزيـزي 🫂\n⌔ تـم حفظ البصمه الذاتية .. تلقائياً ☑️** ❝\n**⌔ معلومـات المـرسـل :-**\n**• الاسم :** {_format.mentionuser(sender.first_name , sender.id)}\n**• اليوزر :** {username}\n**• الايدي :** `{sender.id}`")
